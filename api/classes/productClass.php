3102992ccc5c77d5abe6347d3aded7da68878a7c
SanazShahed <89834396+Sanaz-Shahed@users.noreply.github.com>
2022-02-17 11:57:28 +0100

    Create product

---
+++ api/classes/productClass.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Product {
+    public $id;
+    public $name;
+    public $quantity;
+    public $price;
+    public $picture
+    public $description;
+    public $db
+    
+}
+
+
+
+?>
