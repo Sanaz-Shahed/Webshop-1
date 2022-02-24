47653c062a8ca6e0b052693ab762eda553a98a95
donyateimouri <donyateimouri1@gmail.com>
2022-02-24 09:23:49 +0100

    update order

--- api/classes/order.php
+++ api/classes/order.php
@@ -7,6 +7,18 @@ class Order {
     public $customerID;
     public $date;
     public $sum;
+}
+
+    function __construct($orderID, $unique_code, $shipperID, $customerID,$date,$sum) {
+        $this->orderID = $orderID;
+        $this->unique_code = $unique_code;
+        $this->shipperID = $shipperID;
+        $this->customerID = $customerID;
+        $this->date = $date;
+        $this->sum = $sum;
+    }
+}
+
+?>
 
   
-}
